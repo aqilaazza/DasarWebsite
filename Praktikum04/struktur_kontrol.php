f6c429d552df798f12d19236f7d185e2433bc85d
aqilaazza <aqilanurazza123@gmail.com>
2024-09-22 18:40:34 +0700

    update

--- Praktikum04/struktur_kontrol.php
+++ Praktikum04/struktur_kontrol.php
@@ -50,3 +50,58 @@ foreach($skorUjian as $skor) {
 
 echo "Total skor ujian adalah : $totalSkor";
 
+echo"<br>Menentukan Skor ujian dengan foreach <br>";
+
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+foreach($nilaiSiswa as $nilai) {
+     if ($nilai < 60) {
+        echo "Nilai : $nilai (Tidak Lulus) <br>";
+        continue;
+     }
+
+    echo "Nilai: $nilai (Lulus) <br>";
+}
+
+echo"<br>Studi kasus menghitung total nilai <br>";
+$nilaiSiswa = [85, 92, 78, 64, 90, 75, 88, 79, 70, 96];
+
+//Menentukan nilai terendah ke tertinggi 
+sort($nilaiSiswa);
+
+$totalNilai = 0;
+for($k = 2; $k < count($nilaiSiswa)-2; $k++) {
+    $totalNilai += $nilaiSiswa[$k];
+}
+
+$jumlahSiswa = count($nilaiSiswa) - 4;
+$nilaiRata2 = $totalNilai / $jumlahSiswa;
+
+echo "Jumlah siswa : $jumlahSiswa <br>";
+echo "Total rata-rata (setelah mengabaikan 2 nilai terbesar dan terkecil) : $nilaiRata2 ";
+
+echo "<br> Penjualan produk <br>";
+$totalBelanja = 120000;
+
+if ($totalBelanja > 100000) {
+    $potonganHarga = $totalBelanja * 20/100;
+    $totalBayar = $totalBelanja - $potonganHarga;
+    echo "Total Belanja : Rp " . number_format($totalBelanja, 0, ',', '.') . "<br>";
+    echo "Diskon : 20% <br>";
+    echo "Total Bayar : Rp " . number_format($totalBayar, 0, ',', '.') . "<br>";
+} else {
+    $totalBayar = $totalBelanja;
+    echo "Total Belanja : Rp " . number_format($totalBelanja, 0, ',', '.') . "<br>";
+    echo "Diskon : 0% <br>";
+    echo "Total Bayar : Rp " . number_format($totalBayar, 0, ',', '.') . "<br>";
+}
+
+echo "<br> Bermain game <br>";
+$totalSkor = 100;
+$batasan =500;
+
+$hadiahTambahan = ($totalSkor > $batasan)? 'Ya': 'Tidak';
+
+echo "Total skor pemain adalah : $totalSkor <br>";
+echo "Apakah pemain mendapatkan hadiah tambahan? $hadiahTambahan";
+
