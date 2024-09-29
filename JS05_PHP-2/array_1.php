f79596978ebcbdd5c2eeddc0c0222ac4010df545
aqilaazza <aqilanurazza123@gmail.com>
2024-09-29 17:53:32 +0700

    tanpa loop

---
+++ JS05_PHP-2/array_1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    </head>
+    <body>
+        <h2>Array Terindeks</h2>
+        <?php
+        $Listdosen = ["Elok Nur Hamdan", "Unggul Pemenang", "Bagas Nugraha"];
+
+     
+
+        echo $Listdosen[2] . "<br>";
+        echo $Listdosen[0] . "<br>";
+        echo $Listdosen[1] . "<br>";
+        ?>
+    </body>
+</html>
