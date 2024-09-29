99b28db963289cb01bfc67a4f84e4d1e9f2e304a
aqilaazza <aqilanurazza123@gmail.com>
2024-09-29 18:00:16 +0700

    menggunakan looping

--- JS05_PHP-2/array_1.php
+++ JS05_PHP-2/array_1.php
@@ -7,11 +7,15 @@
         <?php
         $Listdosen = ["Elok Nur Hamdan", "Unggul Pemenang", "Bagas Nugraha"];
 
-     
-
+        echo "<h5>Tanpa Looping</h5>";
         echo $Listdosen[2] . "<br>";
         echo $Listdosen[0] . "<br>";
         echo $Listdosen[1] . "<br>";
+
+        echo "<h5>Menggunakan Looping</h5>";
+        for ($i = 0; $i < count($Listdosen) ; $i++) {
+            echo $Listdosen [$i] . "<br>";
+        }
         ?>
     </body>
 </html>
