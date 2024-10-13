8402d5a37bb0b4e4753a23ff2d5227eeb6271fc3
aqilaazza <aqilanurazza123@gmail.com>
2024-10-13 22:16:50 +0700

    soal 1.2 isset dengan array

--- JS07_FROM PROCESSING/isset.php
+++ JS07_FROM PROCESSING/isset.php
@@ -1,7 +1,7 @@
 <?php
-$umur;
-if (isset($umur) && $umur >= 18) {
-    echo "Anda sudah dewasa.";
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama : ".$data["nama"];
 } else {
     echo "Anda belum dewasa atau variabel 'umur' tidak ditentukan.";
 }
