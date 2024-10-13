d34261528baa2f0a3f44ce13e9ee33e960dce445
aqilaazza <aqilanurazza123@gmail.com>
2024-10-13 22:31:48 +0700

    soal 2.2 empty dengan variable benilai null

--- JS07_FROM PROCESSING/empry.php
+++ JS07_FROM PROCESSING/empry.php
@@ -1,8 +1,14 @@
 <?php
 $myArray = array(); //array kosong
 if (empty($myArray)) {
-    echo "Array tidak terdefinisi atau kosong.";
+    echo "Array tidak terdefinisi atau kosong. <br>";
 } else {
     echo "Array terdefinisi dan tidak kosong";
+}
+
+if (empty($nonExixtentVar)) {
+    echo "Variable tidak terdefinisi atau kosong.";
+} else {
+    echo "Variable terdefinisi dan tidak kosong";
 }
 ?>
