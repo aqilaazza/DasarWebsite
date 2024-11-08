3bce8404bdbb9dbe80e1687fc93e927d201aa7b3
aqilaazza <aqilanurazza123@gmail.com>
2024-11-09 00:45:22 +0700

    update

---
+++ JS10_CRUD/edit.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Edit Data Anggota</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
+// Menghubungkan ke database
+include "koneksi.php";
+
+// Mengambil data anggota berdasarkan ID
+$id = $_GET['id'];
+$query = "SELECT * FROM anggota WHERE id='$id'";
+$result = mysqli_query($koneksi, $query);
+$row = mysqli_fetch_assoc($result);
+mysqli_close($koneksi);
+?>
+
+<div class="container">
+    <h2>Edit Data Anggota</h2>
+    <form action="proses.php?aksi=ubah" method="post">
+        <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+        <label for="nama">Nama:</label>
+        <input type="text" name="nama" id="nama" value="<?php echo $row['nama']; ?>" required>
+        <label for="jenis_kelamin">Jenis Kelamin:</label>
+        <div class="radio-group">
+            <input type="radio" name="jenis_kelamin" value="laki" id="laki" <?php if($row['jenis_kelamin'] == 'laki') echo 'checked'; ?>><label for="laki">Laki-laki</label>
+            <input type="radio" name="jenis_kelamin" value="perempuan" id="perempuan" <?php if($row['jenis_kelamin'] == 'perempuan') echo 'checked'; ?>><label for="perempuan">Perempuan</label>
+        </div>
+        <label for="alamat">Alamat:</label>
+        <input type="text" name="alamat" id="alamat" value="<?php echo $row['alamat']; ?>" required>
+        <label for="no_telp">No. Telepon:</label>
+        <input type="text" name="no_telp" id="no_telp" value="<?php echo $row['no_telp']; ?>" required>
+        <button type="submit">Simpan Perubahan</button>
+        <a href="index.php" class="btn-kembali">Kembali</a>
+    </form>
+</div>
+
+</body>
+</html>
